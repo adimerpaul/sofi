3854a5f3468bf1289e84e9d4bb773bd850d37adb
AFLopezG <lopez.gutierrez.a07@gmail.com>
2022-01-25 23:22:16 -0400

    inicio

---
+++ sofiback/app/Http/Controllers/UserController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        return DB::SELECT('SELECT CodAut,ci,Nombre1,Nombre2,App1,Apm from personal');
+    }
+
+    public function login(Request $request){
+        $validar= DB::SELECT("SELECT * from personal where ci='$request->ci' and pasw ='$request->pass' ");
+        if(sizeof($validar)==1)
+            echo 'Correcto';
+        else    
+            echo 'No existe';
+
+    }
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
