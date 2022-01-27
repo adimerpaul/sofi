c3bee1970579b573ee89012418ce4d03575c29b8
AFLopezG <lopez.gutierrez.a07@gmail.com>
2022-01-26 23:15:18 -0400

    avance

--- sofiback/routes/api.php
+++ sofiback/routes/api.php
@@ -19,5 +19,6 @@ use Illuminate\Support\Facades\Route;
 //});
 Route::post('/login',[\App\Http\Controllers\UserController::class,'login']);
 Route::resource('/cliente',\App\Http\Controllers\ClienteController::class);
+Route::resource('/user',\App\Http\Controllers\UserController::class);
 Route::resource('/asignar',\App\Http\Controllers\AsignarController::class);
 
--- sofifront/src/pages/Asignar.vue
+++ sofifront/src/pages/Asignar.vue
@@ -8,7 +8,7 @@
     <q-btn color="info" icon="search" label="consulta" size="xs" />
   </div>
   <div class="col-4 flex flex-center">
-    <q-btn color="positive" icon="add_circle" label="Asignar" size="xs" />
+    <q-btn color="positive" icon="add_circle" label="Asignar" size="xs" @click="dialog_ag=true"/>
   </div>
   <div class="col-12">
     <q-table dense title="Asignaciones" :columns="columns" :rows="asignaciones">
@@ -19,6 +19,32 @@
       </template>
     </q-table>
   </div>
+      <q-dialog v-model="dialog_ag" >
+      <q-card style="min-width: 350px">
+        <q-card-section>
+          <div class="text-h6">Asignar</div>
+        </q-card-section>
+
+        <q-card-section class="q-pt-none">
+          <q-input outlined label="Fecha" v-model="fecha2"/>
+          <q-select outlined v-model="cliente" 
+                        use-input
+              input-debounce="0"
+              :options="options"
+              @filter="filterFn" label="Clientes" />
+            <q-select outlined v-model="user" 
+                        use-input
+              input-debounce="0"
+              :options="options2"
+              @filter="filterFn2" label="Personal" />
+        </q-card-section>
+
+        <q-card-actions align="right" class="text-primary">
+          <q-btn flat label="Cancel" v-close-popup />
+          <q-btn flat label="Add address" v-close-popup />
+        </q-card-actions>
+      </q-card>
+    </q-dialog>
 </div>
 </q-page>
 </template>
@@ -29,6 +55,15 @@ export default {
   data(){
     return{
       asignaciones:[],
+      asignar:{},
+      cliente:{label:''},
+      fecha2:date.formatDate(Date.now(),'YYYY-MM-DD'),
+      user:{label:''},
+      clientes:[],
+      usuarios:[],
+      options:[],
+      options2:[],
+      dialog_ag:false,
       columns:[
         {label:'fecha',name:'fecha',field:'fecha'},
         {label:'personal',name:'personal',field:'personal'},
@@ -39,8 +74,60 @@ export default {
   },
   created() {
     this.misasignaciones()
+    this.misclientes()
+    this.misusuarios()
   },
   methods:{
+    misusuarios(){
+      this.usuarios=[]
+      this.$api.get('user').then(res=>{
+        console.log(res.data)
+        res.data.forEach(element => {
+          this.usuarios.push({label:element.ci+' '+element.Nombre1+' '+element.Nombre2+' '+element.App1+' '+element.Apm,id:element.CodAut})
+        });
+      })
+
+    },
+        filterFn (val, update) {
+      if (val === '') {
+        update(() => {
+          this.options = this.clientes
+          // with Quasar v1.7.4+
+          // here you have access to "ref" which
+          // is the Vue reference of the QSelect
+        })
+        return
+      }
+      update(() => {
+        const needle = val.toLowerCase()
+        this.options = this.clientes.filter(v => v.label.toLowerCase().indexOf(needle) > -1)
+      })
+    },
+            filterFn2 (val, update) {
+      if (val === '') {
+        update(() => {
+          this.options2 = this.usuarios
+          // with Quasar v1.7.4+
+          // here you have access to "ref" which
+          // is the Vue reference of the QSelect
+        })
+        return
+      }
+      update(() => {
+        const needle = val.toLowerCase()
+        this.options2 = this.usuarios.filter(v => v.label.toLowerCase().indexOf(needle) > -1)
+      })
+    },
+    misclientes(){
+      this.clientes=[]
+      this.$api.get('cliente').then(res=>{
+        console.log(res.data)
+        res.data.forEach(element => {
+          this.clientes.push({label:element.Id+' '+element.Nombres,id:element.Cod_Aut})
+        });
+      })
+
+    },
     misasignaciones(){
       this.$api.get('asignar/'+this.fecha).then(res=>{
         console.log(res.data)
