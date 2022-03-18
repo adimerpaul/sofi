d7bf11c3e6bae7731209c8e0deda7df3d42594ba
adimerpaul <adimer101@email.com>
2022-03-18 17:47:42 -0400

    Update PedidoController.php

--- sofiback/app/Http/Controllers/PedidoController.php
+++ sofiback/app/Http/Controllers/PedidoController.php
@@ -433,7 +433,7 @@ class PedidoController extends Controller
             tbproductos.Producto as nombre
             from tbpedidos,tbproductos where tbpedidos.cod_prod=tbproductos.cod_prod and  NroPed = '$row->NroPed'" );
 
-            array_push($total,$lisrped);
+//            array_push($total,$lisrped);
         }
 //        $row->pedidos=$total;
         return $pedido;
