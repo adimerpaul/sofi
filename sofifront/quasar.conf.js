5fa435aeda548ff4f51e033be04d97710f146e15
AFLopezG <lopez.gutierrez.a07@gmail.com>
2022-03-22 22:32:39 -0400

    Revert "subir"
    This reverts commit c9c21e7c6c8716d75c0009b0390d60457a2313ce.

--- sofifront/quasar.conf.js
+++ sofifront/quasar.conf.js
@@ -73,7 +73,7 @@ module.exports = configure(function (ctx) {
       },
       env: {
         API: ctx.dev
-          ? 'http://localhost:8000/api/'
+          ? 'https://sofiab.loca.lt/api/'
           : 'http://165.227.143.191:83/api/'
       }
     },
