46983b8e3847d5718adfccfd26ab3044d0ce068e
AFLopezG <lopez.gutierrez.a07@gmail.com>
2022-11-11 10:05:25 -0400

    subit

--- sofiback/app/Http/Controllers/ProductoController.php
+++ sofiback/app/Http/Controllers/ProductoController.php
@@ -14,12 +14,12 @@ class ProductoController extends Controller
      */
     public function index()
     {
-        return DB::select("SELECT cod_prod,Producto,Precio,codUnid,tipo,0 cantidad FROM `tbproductos` ");
-       /* return DB::select("SELECT p.cod_prod,p.Producto,p.Precio,p.codUnid,p.tipo,
+        //return DB::select("SELECT cod_prod,Producto,Precio,codUnid,tipo,0 cantidad FROM `tbproductos` ");
+        return DB::select("SELECT p.cod_prod,p.Producto,p.Precio,p.codUnid,p.tipo,
         (SUM(s.cant)-SUM(s.saldo)) cantidad
         FROM tbproductos p
         INNER JOIN tbstock s ON s.cod_prod=p.cod_prod
-        GROUP BY p.cod_prod,p.Producto,p.Precio,p.codUnid,p.tipo");*/
+        GROUP BY p.cod_prod,p.Producto,p.Precio,p.codUnid,p.tipo");
     }
 
     public function listProducto(){
@@ -34,7 +34,7 @@ class ProductoController extends Controller
         return DB::select("SELECT p.cod_prod,p.Producto,p.Precio,p.Precio_Costo,p.Precio3,p.Precio4,p.Precio5,p.Precio6,p.Precio7,p.Precio8,p.Precio9,p.Precio10,p.Precio11,p.Precio12,p.Precio13,p.PreCosto,
         p.codUnid,p.tipo
         FROM tbproductos p
-                ");  
+                ");
     }
     /**
      * Store a newly created resource in storage.
