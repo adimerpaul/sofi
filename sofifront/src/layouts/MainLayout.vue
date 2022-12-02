48172656e08c3bf7760d5e8a23b37510eadd184d
AFLopezG <lopez.gutierrez.a07@gmail.com>
2022-12-02 13:02:18 -0400

    mod presnt

--- sofifront/src/layouts/MainLayout.vue
+++ sofifront/src/layouts/MainLayout.vue
@@ -134,7 +134,7 @@
           <q-item-section>
             <q-item-label>Cliente No Pedido</q-item-label>
             <q-item-label caption>
-              No pedido 
+              No pedido
             </q-item-label>
           </q-item-section>
         </q-item>
@@ -260,7 +260,7 @@ export default {
       leftDrawerOpen:false,
       vendores:['12612870','1593578','33555433','3520335','5676554','7422201','9876785','7360035','5067737','7331330','7308976','7377278'],
       encargados:['0' ,'123321' ,'22222222','7205489'],
-      cobrador:['7424457'],
+      cobrador:['7424479'],
       despachador:['7386961','9688418','7205489','7417239','12810781','7395208']
     }
   },
