7ec33badf202ccf0e5bf9dc8665205902641642f
AFLopezG <lopez.gutierrez.a07@gmail.com>
2023-01-27 09:13:25 -0400

    cambio

--- sofiback/routes/api.php
+++ sofiback/routes/api.php
@@ -21,7 +21,8 @@ Route::post('/login',[\App\Http\Controllers\UserController::class,'login']);
 Route::post('/ctacobrar',[\App\Http\Controllers\CobrarController::class,'ctacobrar']);
 Route::resource('/excel',\App\Http\Controllers\ExcelController::class);
 Route::get('/excel/{t}/{f1}/{f2}/{CodAut}',[\App\Http\Controllers\ExcelController::class,'consulta']);
-Route::group(['middleware'=>'auth:sanctum'],function (){
+    Route::post('/listsinpedido',[\App\Http\Controllers\ClienteController::class,'listsinpedido']);
+    Route::group(['middleware'=>'auth:sanctum'],function (){
     Route::post('/me',[\App\Http\Controllers\UserController::class,'me']);
     Route::resource('/cliente',\App\Http\Controllers\ClienteController::class);
     Route::resource('/user',\App\Http\Controllers\UserController::class);
