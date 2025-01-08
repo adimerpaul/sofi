4dcf51ae402ce3ff60c68725c6136d477c06f4e6
adimer <adimer101@gmail.com>
2025-01-08 04:15:34 -0400

    Actulizado mapa por vendedor

--- sofifront/src/pages/MapaVendedor/MapaVendedor.vue
+++ sofifront/src/pages/MapaVendedor/MapaVendedor.vue
@@ -21,7 +21,7 @@
                   </tr>
                 </thead>
                 <tbody>
-                  <tr v-for="(user, index) in pedidos.users" :key="index">
+                  <tr v-for="(user, index) in pedidos.users" :key="index" @click="seleccionar(user)">
                     <td>{{ index + 1 }}</td>
                     <td>{{ user.nombreCompleto }}</td>
                     <td>
@@ -64,15 +64,8 @@
                 <LTileLayer
                   url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 ></LTileLayer>
-<!--                <l-marker v-for="(c,i) in pedidos?.users" :key="i" :lat-lng="[c.cliente?.Latitud, c.cliente?.longitud]">-->
-<!--                  <l-tooltip :content="c.Nombres"></l-tooltip>-->
-<!--                  <l-icon >-->
-<!--                    <q-badge style="padding: 2px" >{{c.idCli}}-->
-<!--                    </q-badge>-->
-<!--                  </l-icon>-->
-<!--                </l-marker>-->
-                <template v-for="(user) in pedidos?.users">
-                  <l-marker v-for="(pedido,i) in user?.pedidos?.pedidos" :key="i" :lat-lng="[pedido.pedido.cliente.Latitud, pedido.pedido.cliente.longitud]">
+                <template v-for="(user) in pedidosMapa?.users">
+                  <l-marker v-for="(pedido,i) in user?.pedidos?.pedidos" :key="i" :lat-lng="[parseFloat(pedido.pedido.cliente.Latitud), parseFloat(pedido.pedido.cliente.longitud)]">
                     <l-tooltip :content="pedido.pedido.cliente.Nombres">
                     </l-tooltip>
                     <l-icon >
@@ -83,200 +76,18 @@
                   </l-marker>
                 </template>
               </l-map>
+<!--              <template v-for="(user,i) in pedidosMapa?.users" :key="i">-->
+<!--                <div v-for="(pedido,j) in user?.pedidos?.pedidos" :key="j">-->
+<!--                  <q-badge style="padding: 2px" :color="pedido.pedido.estado === 'CREADO' ? 'green' : 'blue'">-->
+<!--                    {{i++}} {{pedido.pedido.cliente.Latitud}}  {{pedido.pedido.cliente.longitud}}-->
+<!--                  </q-badge>-->
+<!--                </div>-->
+<!--              </template>-->
             </div>
           </div>
-          <div class="col-12 col-md-12 ">
-            <pre>{{pedidos}}</pre>
-<!--            {-->
-<!--            "users": [-->
-<!--            {-->
-<!--            "CodAut": "28",-->
-<!--            "nombreCompleto": "JUAN RIOS",-->
-<!--            "pedidos": {-->
-<!--            "cantidad": 3,-->
-<!--            "enviados": 0,-->
-<!--            "creados": 3,-->
-<!--            "pedidos": [-->
-<!--            {-->
-<!--            "pedido": {-->
-<!--            "NroPed": 110566,-->
-<!--            "fecha": "2025-01-08 11:49:45",-->
-<!--            "idCli": "1709",-->
-<!--            "CIfunc": "28",-->
-<!--            "estado": "CREADO",-->
-<!--            "fact": "NO",-->
-<!--            "comentario": null,-->
-<!--            "pago": "CREDITO",-->
-<!--            "cliente": {-->
-<!--            "Cod_Aut": 1709,-->
-<!--            "Nombres": "PAMELA NICKOL MIRANDA CANAZA",-->
-<!--            "Direccion": "BRASIL Y BOLIVAR PLENA ESQ.",-->
-<!--            "Telf": "76365165",-->
-<!--            "zona": "CENTRAL",-->
-<!--            "Latitud": "-17.9723975",-->
-<!--            "longitud": "-67.1069087"-->
-<!--            },-->
-<!--            "user": {-->
-<!--            "CodAut": 28,-->
-<!--            "Nombre1": "JUAN",-->
-<!--            "App1": "RIOS",-->
-<!--            "ci": "7360035"-->
-<!--            }-->
-<!--            },-->
-<!--            "productos": [-->
-<!--            {-->
-<!--            "NroPed": 110566,-->
-<!--            "cod_prod": "533329",-->
-<!--            "precio": "46.80",-->
-<!--            "Cant": "6.00",-->
-<!--            "Canttxt": "",-->
-<!--            "subtotal": "280.80",-->
-<!--            "producto": {-->
-<!--            "cod_prod": "533329",-->
-<!--            "Producto": "CHORIZO CHURRASQUERO AL VACIO DE 1 KG                                                               "-->
-<!--            }-->
-<!--            }-->
-<!--            ]-->
-<!--            },-->
-<!--            {-->
-<!--            "pedido": {-->
-<!--            "NroPed": 110728,-->
-<!--            "fecha": "2025-01-08 11:32:08",-->
-<!--            "idCli": "1845",-->
-<!--            "CIfunc": "28",-->
-<!--            "estado": "CREADO",-->
-<!--            "fact": "NO",-->
-<!--            "comentario": null,-->
-<!--            "pago": "CONTADO",-->
-<!--            "cliente": {-->
-<!--            "Cod_Aut": 1845,-->
-<!--            "Nombres": "JENNY XIMENA CHURA NINA",-->
-<!--            "Direccion": "MERCADO  CAMPERO",-->
-<!--            "Telf": "73846886",-->
-<!--            "zona": "CENTRO",-->
-<!--            "Latitud": "-17.9707683",-->
-<!--            "longitud": "-67.1102802"-->
-<!--            },-->
-<!--            "user": {-->
-<!--            "CodAut": 28,-->
-<!--            "Nombre1": "JUAN",-->
-<!--            "App1": "RIOS",-->
-<!--            "ci": "7360035"-->
-<!--            }-->
-<!--            },-->
-<!--            "productos": {-->
-<!--            "2": {-->
-<!--            "NroPed": 110728,-->
-<!--            "cod_prod": "533329",-->
-<!--            "precio": "46.80",-->
-<!--            "Cant": "5.00",-->
-<!--            "Canttxt": "",-->
-<!--            "subtotal": "234.00",-->
-<!--            "producto": {-->
-<!--            "cod_prod": "533329",-->
-<!--            "Producto": "CHORIZO CHURRASQUERO AL VACIO DE 1 KG                                                               "-->
-<!--            }-->
-<!--            }-->
-<!--            }-->
-<!--            },-->
-<!--            {-->
-<!--            "pedido": {-->
-<!--            "NroPed": 110750,-->
-<!--            "fecha": "2025-01-08 14:20:12",-->
-<!--            "idCli": "318",-->
-<!--            "CIfunc": "28",-->
-<!--            "estado": "CREADO",-->
-<!--            "fact": "NO",-->
-<!--            "comentario": null,-->
-<!--            "pago": "CONTADO",-->
-<!--            "cliente": {-->
-<!--            "Cod_Aut": 318,-->
-<!--            "Nombres": "LUZ MARIA ROCHA GONZALEZ",-->
-<!--            "Direccion": "INTERIOR MERCADO CAMPERO",-->
-<!--            "Telf": "69295987",-->
-<!--            "zona": "CENTRO",-->
-<!--            "Latitud": "-17.9706412",-->
-<!--            "longitud": "-67.1101146"-->
-<!--            },-->
-<!--            "user": {-->
-<!--            "CodAut": 28,-->
-<!--            "Nombre1": "JUAN",-->
-<!--            "App1": "RIOS",-->
-<!--            "ci": "7360035"-->
-<!--            }-->
-<!--            },-->
-<!--            "productos": {-->
-<!--            "6": {-->
-<!--            "NroPed": 110750,-->
-<!--            "cod_prod": "533329",-->
-<!--            "precio": "46.80",-->
-<!--            "Cant": "4.00",-->
-<!--            "Canttxt": "",-->
-<!--            "subtotal": "187.20",-->
-<!--            "producto": {-->
-<!--            "cod_prod": "533329",-->
-<!--            "Producto": "CHORIZO CHURRASQUERO AL VACIO DE 1 KG                                                               "-->
-<!--            }-->
-<!--            }-->
-<!--            }-->
-<!--            }-->
-<!--            ]-->
-<!--            }-->
-<!--            },-->
-<!--            {-->
-<!--            "CodAut": "43",-->
-<!--            "nombreCompleto": "DANIELA LOPEZ",-->
-<!--            "pedidos": {-->
-<!--            "cantidad": 1,-->
-<!--            "enviados": 0,-->
-<!--            "creados": 1,-->
-<!--            "pedidos": [-->
-<!--            {-->
-<!--            "pedido": {-->
-<!--            "NroPed": 110648,-->
-<!--            "fecha": "2025-01-08 17:29:17",-->
-<!--            "idCli": "3428",-->
-<!--            "CIfunc": "43",-->
-<!--            "estado": "CREADO",-->
-<!--            "fact": "NO",-->
-<!--            "comentario": null,-->
-<!--            "pago": "CONTADO",-->
-<!--            "cliente": {-->
-<!--            "Cod_Aut": 3428,-->
-<!--            "Nombres": "CARMEN GABRIELA MAMANI SANTOS",-->
-<!--            "Direccion": "Adolfo Mier entre Tejerina y Brasil acera sud",-->
-<!--            "Telf": "77151223",-->
-<!--            "zona": "CENTRO",-->
-<!--            "Latitud": "-17.97174",-->
-<!--            "longitud": "-67.10590"-->
-<!--            },-->
-<!--            "user": {-->
-<!--            "CodAut": 43,-->
-<!--            "Nombre1": "DANIELA",-->
-<!--            "App1": "LOPEZ",-->
-<!--            "ci": "5773491"-->
-<!--            }-->
-<!--            },-->
-<!--            "productos": {-->
-<!--            "1": {-->
-<!--            "NroPed": 110648,-->
-<!--            "cod_prod": "650303",-->
-<!--            "precio": "279.10",-->
-<!--            "Cant": "5.00",-->
-<!--            "Canttxt": "P2",-->
-<!--            "subtotal": "1395.50",-->
-<!--            "producto": {-->
-<!--            "cod_prod": "650303",-->
-<!--            "Producto": "P.U. 21 KG MICAN CACHORRO CANINO                                                                    "-->
-<!--            }-->
-<!--            }-->
-<!--            }-->
-<!--            }-->
-<!--            ]-->
-<!--            }-->
-<!--            },-->
-
-          </div>
+<!--          <div class="col-12 col-md-12 ">-->
+<!--            <pre>{{pedidosMapa}}</pre>-->
+<!--          </div>-->
         </div>
       </q-card-section>
     </q-card>
@@ -302,16 +113,23 @@ export default {
       fecha: moment().format("YYYY-MM-DD"),
       loading: false,
       pedidos: [],
+      pedidosAll: [],
+      pedidosMapa: [],
     };
   },
   mounted() {
     this.buscar();
   },
   methods: {
+    seleccionar(user) {
+      this.pedidosMapa.users = this.pedidosAll.users.filter((u) => u.CodAut === user.CodAut)
+    },
     buscar() {
       this.loading = true;
       this.$api.post("mapaVendedor", { fecha: this.fecha }).then((res) => {
         this.pedidos = res.data;
+        this.pedidosMapa = {...res.data};
+        this.pedidosAll = {...res.data};
       }).finally(() => {
         this.loading = false;
       });
